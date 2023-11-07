e5aa2342a5e3fa00615404f2a7a5515db8dbc360
TechDanny <kokidanny15@gmail.com>
2023-11-07 13:24:42 -0800

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """if not valid return 0"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My user Agent 1.0"}
-    response = requets.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json().get('data', {})
