168a784e8f705c00978e954c0ab0dc31dbe3fbcc
TechDanny <kokidanny15@gmail.com>
2023-11-07 14:04:09 -0800

    recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Queries Reddit API and returns the list containing all the titles
+"""
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """if not valid return None"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    rslt = response.json().get('data')
+    after = rslt.get('after')
+    count += rslt.get('dist')
+    for i in rslt.get("children"):
+        my_list.append(i.get('data').get('title'))
+
+    if after is not None:
+        return recurse(subreddit, my_list, after, count)
+    else:
+        return my_list
